dbc03d1679db0601c67590d6fd656af43a1a12ed
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-26 12:38:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   
+<<<<<<< HEAD
 
   attr_accessor :name, :artist
   def initialize(name)
@@ -19,5 +20,25 @@ class Song
   artist.add_song(song)
     binding.pry
 return song
+=======
+  @@all = []
+  attr_accessor :name, :artist, :all
+  def initialize(name)
+    @name = name
+    @song = []
+  end
+  
+  def self.all
+    
+    @@all
+  end
+  
+  def new_by_filename(title)
+   
+    song = Song.new(title.split(" - ")[1])
+    song.artist = title.split(" - ")[0]
+    @@all << song
+    @song << song
+>>>>>>> d8ff5ca16400ed050ab0e37a83e8c5078dbbe760
   end
 end
